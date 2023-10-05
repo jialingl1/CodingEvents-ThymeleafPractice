3de01ead7dc3a82579d14b55d57991c582a8ab64
Jialing Liu <jialingl.liu@gmail.com>
2023-10-05 13:08:35 -0400

    added data layer and refactor code to use it.

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,5 +1,6 @@
 package org.launchcode.codingevents.controllers;
 
+import org.launchcode.codingevents.data.EventData;
 import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,14 +16,14 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
-    private static List<Event> events = new ArrayList<>();
+    //private static List<Event> events = new ArrayList<>();
 
     @GetMapping
     public String displayAllEvents(Model model) {
 //        List<String> events = new ArrayList<>();
 //        events.add("Halloween");
 //        events.add("Thanksgiving");
-        model.addAttribute("events", events);
+        model.addAttribute("events", EventData.getAll());
         return "events/index";
     }
 
@@ -35,7 +36,7 @@ public class EventController {
     //route: events/create
     @PostMapping("create")
     public String createEvent(@RequestParam String eventName, @RequestParam String eventDescription){
-        events.add(new Event(eventName, eventDescription));
+        EventData.add(new Event(eventName, eventDescription));
         return "redirect:/events";
     }
 
