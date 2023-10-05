75e04e29b3086bbb0802dff06c0d0790a8db9cfc
Jialing Liu <jialingl.liu@gmail.com>
2023-10-05 15:24:08 -0400

    added delete event.

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -23,13 +23,15 @@ public class EventController {
 //        List<String> events = new ArrayList<>();
 //        events.add("Halloween");
 //        events.add("Thanksgiving");
+        model.addAttribute("title", "All Events");
         model.addAttribute("events", EventData.getAll());
         return "events/index";
     }
 
     //route: events/create
     @GetMapping("create")
-    public String renderCreateEventForm() {
+    public String renderCreateEventForm(Model model) {
+        model.addAttribute("title", "Create Event");
         return "events/create";
     }
 
@@ -38,6 +40,24 @@ public class EventController {
     public String createEvent(@RequestParam String eventName, @RequestParam String eventDescription){
         EventData.add(new Event(eventName, eventDescription));
         return "redirect:/events";
+    }
+
+    @GetMapping("delete")
+    public String displayDeleteEventForm(Model model) {
+        model.addAttribute("title", "Delete Events");
+        model.addAttribute("events", EventData.getAll());
+        return "events/delete";
+    }
+
+    @PostMapping("delete")
+    public String processDeleteEventsForm(@RequestParam(required = false) int[] eventIds){
+        if (eventIds != null) {
+            for (int id : eventIds) {
+                EventData.remove(id);
+            }
+        }
+        return "redirect:/events";
+
     }
 
 }
